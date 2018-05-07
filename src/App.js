4152cb07a6cb0260122d9399367446965f096f9d
Diana Lease <dianarlease@gmail.com>
2018-05-07 18:15:02 -0400

    uses dropdown for data types  and updates state to use parameters array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Button, Form, Header, Input, Label, } from 'semantic-ui-react';
-import CtoModelModal from './CtoModelModal';
+import { Button, Dropdown, Form, Header, Input, Label, } from 'semantic-ui-react';
 
 // import auth0 from 'auth0-js';
 
@@ -28,7 +27,7 @@ class App extends React.Component {
 
     this.renderText = this.renderText.bind(this);
     this.extractKeywords = this.extractKeywords.bind(this);
-    this.createContentControl = this.createContentControl.bind(this);
+    this.highlightVars = this.highlightVars.bind(this);
     this.createBindingFromSelection = this.createBindingFromSelection.bind(this);  
     this.handleChangeName = this.handleChangeName.bind(this);
     this.goToBinding = this.goToBinding.bind(this);
@@ -42,7 +41,6 @@ class App extends React.Component {
   }
   
   extractKeywords() {
-    console.log('clicked');
     const that = this;
     
     const text = this.state.selectedText;
@@ -68,12 +66,38 @@ class App extends React.Component {
     this.setState({templateName: event.target.value});
   }
 
-  handleDataTypeChange(varName, event) {
-    this.setState({ dataTypes: {...this.state.dataTypes, [varName]: event.target.value }});
+  handleDataTypeChange(varName, event, data) {  
+    const parameters = this.state.parameters;
+    let paramObj = parameters.find((param) => param.name === varName);
+    if (paramObj) {
+      for (let i in parameters) {
+        if (parameters[i].name === varName) {
+          parameters[i].dataType = data.value;
+          break;
+        }
+        i++;
+      }
+    } else {
+      parameters.push ({ name: varName, dataType: data.value });
+    }
+    this.setState({ parameters });
   }
 
   handleSampleChange(varName, event) {
-    this.setState({ samples: {...this.state.samples, [varName]: event.target.value }});
+    const parameters = this.state.parameters;
+    let paramObj = parameters.find((param) => param.name === varName);
+    if (paramObj) {
+      for (let i in parameters) {
+        if (parameters[i].name === varName) {
+          parameters[i].sample = event.target.value;
+          break;
+        }
+        i++;
+      }
+    } else {
+      parameters.push ({ name: varName, sample: event.target.value });
+    }
+    this.setState({ parameters });
   }
   
   handleSubmit() {
@@ -81,14 +105,15 @@ class App extends React.Component {
     this.setState({ open: false });
   }
 
-  highlightVars(variable) {
+  createBindingForVar(variable) {
+    let searchResults;
     // Run a batch operation against the Word object model.
     window.Word.run(function (context) {
       // Queue a command to search the document with a wildcard
-      var searchResults = context.document.body.search(variable, {matchWildCards: true});
+      searchResults = context.document.body.search(variable.key, {matchWildCards: true});
 
       // Queue a command to load the search results and get the font property values.
-      context.load(searchResults, 'font');
+      context.load(searchResults);
       
       // Synchronize the document state by executing the queued commands, 
       // and return a promise to indicate task completion.
@@ -98,15 +123,30 @@ class App extends React.Component {
 
         // Queue a set of commands to change the font for each found item.
         for (var i = 0; i < searchResults.items.length; i++) {
-          searchResults.items[i].font.highlightColor = 'pink';
-          // searchResults.items[i].font.bold = true;
+          let range = searchResults.items[i];
+          console.log(({range}));
+          let contentControl = range.insertContentControl();
+          let title = `${variable.key} ${i}`;
+          contentControl.title = title;
+          contentControl.tag = variable.key;
+          console.log({contentControl});
         }
-        
+
         // Synchronize the document state by executing the queued commands, 
         // and return a promise to indicate task completion.
         return context.sync();
       });  
     })
+      .then(() => {            
+        for (var j = 0; j < searchResults.items.length; j++) {
+          let title = `${variable.key} ${j}`;
+
+          window.Office.context.document.bindings.addFromNamedItemAsync(title, window.Office.BindingType.Text, { id: title }, function (asyncResult) {
+            console.log('name of item', (title));
+            console.log({ asyncResult });
+          });
+        }
+      })
       .catch(function (error) {
         console.log('Error: ' + JSON.stringify(error));
         if (error instanceof window.OfficeExtension.Error) {
@@ -115,7 +155,7 @@ class App extends React.Component {
       });
   }
 
-  createContentControl(variable) {
+  highlightVars(variable) {
     // Run a batch operation against the Word object model.
     window.Word.run(function (context) {
       // Queue a command to search the document with a wildcard
@@ -132,27 +172,9 @@ class App extends React.Component {
 
         // Queue a set of commands to change the font for each found item.
         for (var i = 0; i < searchResults.items.length; i++) {
-          let range = searchResults.items[i];
-          console.log(({range}));
-          let contentControl = range.insertContentControl();
-          let title = variable.key + i;
-          contentControl.title = title;
-          contentControl.tag = variable.key;
-          console.log({contentControl});
           searchResults.items[i].font.highlightColor = 'pink';
-          // searchResults.items[i].font.bold = true;
         }
         
-        // for (var j = 0; j < searchResults.items.length; j++) {
-        //   let title = (variable.key + j);
-
-        //   window.Office.context.document.bindings.addFromNamedItemAsync(title, window.Office.BindingType.Text, function (asyncResult) {
-        //     console.log('name of item', (title));
-        //     console.log({ asyncResult });
-        //   });
-        // }
-
-
         // Synchronize the document state by executing the queued commands, 
         // and return a promise to indicate task completion.
         return context.sync();
@@ -195,60 +217,51 @@ class App extends React.Component {
     });
   }
 
-  createBindingForVar(variable) {
-    // Run a batch operation against the Word object model.
-    window.Word.run(function (context) {
-      // Queue a command to search the document with a wildcard
-      var searchResults = context.document.body.search(variable.key, {matchWildCards: true});
-      
-      context.load(searchResults);
-
-      // Synchronize the document state by executing the queued commands, 
-      // and return a promise to indicate task completion.
-      return context.sync().then(function () {
-        console.log('Found count: ' + searchResults.items.length);
-        console.log('search results', searchResults.items);
-        
-        for (var j = 0; j < searchResults.items.length; j++) {
-          let title = (variable.key + j);
-
-          window.Office.context.document.bindings.addFromNamedItemAsync(title, window.Office.BindingType.Text, function (asyncResult) {
-            console.log('name of item', (title));
-            console.log({ asyncResult });
-          });
-        }
-
-        // Synchronize the document state by executing the queued commands, 
-        // and return a promise to indicate task completion.
-        return context.sync();
-      });  
-    })
-      .catch(function (error) {
-        console.log('Error: ' + JSON.stringify(error));
-        if (error instanceof window.OfficeExtension.Error) {
-          console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-        }
-      });
-  }
-
   goToBinding(id) {
     window.Office.context.document.goToByIdAsync(id, window.Office.GoToType.Binding);
   }
 
   get ctoModelForm() {
+    const dataTypeOptions = [
+      {
+        text: 'String',
+        value: 'String',
+      },
+      {
+        text: 'Long',
+        value: 'Long',
+      },
+      {
+        text: 'Integer',
+        value: 'Integer',
+      },
+      {
+        text: 'DateTime',
+        value: 'DateTime',
+      },
+      {
+        text: 'Double',
+        value: 'Double',
+      },
+      {
+        text: 'Boolean',
+        value: 'Boolean',
+      },
+    ];
     return (
       <div>
         <Header>Create CTO Model</Header>
         <Form>
           {this.state.variableObjs.map((item, index) => {
             console.log({item});
+            console.log({dataTypeOptions});
             return (
               <Form.Group key={index}>
                 <Label>{item.key}</Label>
-                <Form.Input
-                  type="text"
-                  placeholder="Data Type"
-                  onChange={(event) => this.handleDataTypeChange(item.key, event)}
+                <Dropdown
+                  placeholder="Select Data Type"
+                  options={dataTypeOptions}
+                  onChange={(event, data) => this.handleDataTypeChange(item.key, event, data)}
                 />
                 <Form.Input
                   type="text"
@@ -319,9 +332,9 @@ class App extends React.Component {
                 <span>{variableObj.key}</span>
                 <Button
                   size="tiny"
-                  onClick={() => (this.createContentControl(variableObj))}
+                  onClick={() => (this.highlightVars(variableObj))}
                 >
-                  Create Content Control
+                  Highlight all
                 </Button>
                 <Button
                   size="tiny"
