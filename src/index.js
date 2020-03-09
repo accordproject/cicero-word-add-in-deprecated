63ca5f7f763a64d80a37e743d1f7ae3a4b6744b1
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-10 01:31:22 +0530

    add comments
    Signed-off-by: Shrutika Shrutika <shrutika051220@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//Always boot strap the root Component in Office.initialize
 window.Office.initialize = () => {
   ReactDOM.render(<App />, document.getElementById('root'));
   registerServiceWorker();
