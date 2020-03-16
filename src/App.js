1b928ef9dcd01f17f2ce35b088bd10bd28f165dd
Your Name <arteevraina@gmail.com>
2020-03-16 21:09:35 +0530

    refactor(App.js):refactoring App component into Functional Component
    Signed-off-by: Arteev Raina <arteevraina@gmail.com>
    Signed-off-by: Your Name <arteevraina@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment} from 'react';
 import TopNavigation from './TopNavigation';
-
 import { MuiThemeProvider } from 'material-ui/styles';
 import { withTheme } from 'material-ui/styles'
 
 /**
  * Root component for the app
  */
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
+const App = () => {
     return (
-      <MuiThemeProvider theme={this.props.theme}>
+      <MuiThemeProvider>
         <Fragment>
           <TopNavigation/>
         </Fragment>
       </MuiThemeProvider>
     );
-  }
 }
 
 export default withTheme()(App);
