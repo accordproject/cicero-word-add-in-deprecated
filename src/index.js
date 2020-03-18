ff52e7efa9365eda77aabe6cd180a2dbaf0d0cad
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-18 13:33:51 +0530

    add react hot loader to improve addin performance
    Signed-off-by: Shrutika Shrutika <shrutika051220@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { AppContainer } from "react-hot-loader";
 import registerServiceWorker from './registerServiceWorker';
 
 const Office = window.Office;
 
+let isOfficeInitialized = false;
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component isOfficeInitialized={isOfficeInitialized} />
+    </AppContainer>,
+    document.getElementById("root")
+  );
+};
+
 Office.initialize = () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  isOfficeInitialized = true;
+  render(App);
   registerServiceWorker();
 };
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    const NextApp = require("./App").default;
+    render(NextApp);
+  });
+}
