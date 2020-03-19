5513cb2d2ed8ea1c4fc5b31bcdc1877b4c15da28
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-19 11:31:23 +0530

    update the spinner with Circular Progress from material-ui
    Signed-off-by: Shrutika Shrutika <shrutika051220@gmail.com>

--- src/Progress.js
+++ src/Progress.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import Spinner from 'react-spinner-material';
 import Logo from './assets/logo-filled.png';
 import { MuiThemeProvider } from 'material-ui/styles';
 import { withTheme } from 'material-ui/styles'
+import {Grid, CircularProgress} from '@material-ui/core/';
+
 const Progress = (props) => {
         return (
-          <MuiThemeProvider theme={props.theme}>
-                <div className="spinner">
-                    <img width="20%"  src={Logo} alt={props.title} title={props.title}/>
-                    <Spinner radius={50} color={"#333"} stroke={3} visible={true} />
-                    <h2>Please sideload your addin to see app body.</h2>
-                </div>
-            </MuiThemeProvider>
+              <MuiThemeProvider theme={props.theme}>
+                  <Grid container direction="row" justify="center" alignItems="center">
+                        <div>
+                            <h1 style={{align:"center"}}>Cicero Word Add-in</h1>
+                            <img src={Logo} alt={props.title} title={props.title}/>
+                            <h2>Please sideload your addin in Ms-word to see app body.</h2>
+                            <CircularProgress style={{ marginLeft:"40%"}} color="primary" />
+                            <h3><a href="https://docs.microsoft.com/en-us/office/dev/add-ins/testing/create-a-network-shared-folder-catalog-for-task-pane-and-content-add-ins">Sideload Office Add-ins</a></h3>
+                        </div>
+                  </Grid>
+              </MuiThemeProvider>
         );
     }
 
