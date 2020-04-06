8c284c5a3d5ea3f57532dd201ec74bca15fe7e8f
nik72619c <nikhilsharmarockstar21@gmail.com>
2020-04-06 22:01:03 +0530

    feat(Progress.js): Added logos to the react webpage
    Signed-off-by: nik72619c <nikhilsharmarockstar21@gmail.com>

--- src/Progress.js
+++ src/Progress.js
@@ -3,14 +3,16 @@ import Logo from './assets/logo-filled.png';
 import { MuiThemeProvider } from 'material-ui/styles';
 import { withTheme } from 'material-ui/styles'
 import {Grid, CircularProgress} from '@material-ui/core/';
+import { ReactComponent as AccordLogo } from './assets/accord-project.svg';
+import {ReactComponent as CiceroLogo} from './assets/Cicero-black.svg';
 
 const Progress = (props) => {
         return (
               <MuiThemeProvider theme={props.theme}>
                   <Grid container direction="row" justify="center" alignItems="center">
                         <div>
-                            <h1 style={{align:"center"}}>Cicero Word Add-in</h1>
-                            <img src={Logo} alt={props.title} title={props.title}/>
+                            <h1 style={{align:"center", textAlign: 'center'}}><CiceroLogo className="cicero-logo"/> Word Add-in</h1>
+                            <div className="accord-project-logo"><AccordLogo /></div>
                             <h2>Please <a href="https://docs.microsoft.com/en-us/office/dev/add-ins/testing/create-a-network-shared-folder-catalog-for-task-pane-and-content-add-ins">sideload your add-in</a> in Ms-word to see app body.</h2>
                             <CircularProgress style={{ marginLeft:"40%"}} color="primary" />
                         </div>
